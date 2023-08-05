6da765e413f4edeea21596d332482ce6b4d042d1
Mamut010 <phucthinh0013@gmail.com>
2023-08-05 13:44:17 +0700

    Updated ErrorHandler's logic: separate Prisma error handling logic from other logic

--- src/shared/middlewares/error.middleware.ts
+++ src/shared/middlewares/error.middleware.ts
@@ -11,58 +11,72 @@ import { env } from '../../env';
 import { container } from '../../core/bootstrappers';
 import { INJECTION_TOKENS } from '../../core/constants/injection-tokens';
 import { Logger } from '../../lib/logger';
+import { ErrorResponse } from '../../contracts/responses/error.response';
 
 @Middleware({ type: 'after' })
 @injectable()
 export class ErrorHandler implements ExpressErrorMiddlewareInterface {
   error(error: any, request: Request, response: Response, next: NextFunction) {
-    const status = getErrorStatus(error);
-    const message = getErrorMessage(error);
+    let prismaError = this.handlePrismaError(error);
+    if (prismaError) {
+      response.status(prismaError.status).json(prismaError);
+    }
 
-    response.status(status).json({
+    const status = this.getErrorStatus(error);
+    const message = this.getErrorMessage(error);
+
+    const errorResponse: ErrorResponse = {
       status,
-      message,
-    });
-  }
-}
+      message
+    };
 
-function getErrorStatus(error: any): number {
-  if (Array.isArray(error) && error.length > 0 && error.every(err => err instanceof ValidationError)) {
-    return HTTP_CODES.BadRequest;
+    response.status(status).json(errorResponse);
   }
-  else {
-    const possibleStatusProperties = ['status', 'httpCode'];
-    for(const prop of possibleStatusProperties) {
+
+  private handlePrismaError(error: any): ErrorResponse | undefined {
+    // Swallow database messages in production environment
+    if (env.isProduction && (
+      error instanceof Prisma.PrismaClientInitializationError
+      || error instanceof Prisma.PrismaClientKnownRequestError
+      || error instanceof Prisma.PrismaClientRustPanicError
+      || error instanceof Prisma.PrismaClientUnknownRequestError
+      || error instanceof Prisma.PrismaClientValidationError
+    )) {
+      const logger: Logger = container.get(INJECTION_TOKENS.Logger);
+      logger.error(error.message);
+      return {
+        status: HTTP_CODES.InternalServerError,
+        message: UNEXPECTED_ERROR_MESSAGES.DefaultMessage
+      };
+    }
+
+    return undefined;
+  }
+
+  private getErrorStatus(error: any): number {
+    if (Array.isArray(error) && error.length > 0 && error.every(err => err instanceof ValidationError)) {
+      return HTTP_CODES.BadRequest;
+    }
+    else {
+      const possibleStatusProperties = ['status', 'httpCode'];
+      for(const prop of possibleStatusProperties) {
+        if (error[prop]) {
+          return error[prop] as number;
+        }
+      }
+  
+      return HTTP_CODES.InternalServerError;
+    }
+  }
+
+  private getErrorMessage(error: any): string {
+    const possibleMsgProperties = ['errors', 'message'];
+    for(const prop of possibleMsgProperties) {
       if (error[prop]) {
-        return error[prop] as number;
+        return error[prop] as string;
       }
     }
-
-    return HTTP_CODES.InternalServerError;
-  }
-}
-
-function getErrorMessage(error: any): string {
-  // Swallow database messages on production environment
-  if (env.isProduction && (
-    error instanceof Prisma.PrismaClientInitializationError
-    || error instanceof Prisma.PrismaClientKnownRequestError
-    || error instanceof Prisma.PrismaClientRustPanicError
-    || error instanceof Prisma.PrismaClientUnknownRequestError
-    || error instanceof Prisma.PrismaClientValidationError
-  )) {
-    const logger: Logger = container.get(INJECTION_TOKENS.Logger);
-    logger.error(error.message);
+  
     return UNEXPECTED_ERROR_MESSAGES.DefaultMessage;
   }
-
-  const possibleMsgProperties = ['errors', 'message'];
-  for(const prop of possibleMsgProperties) {
-    if (error[prop]) {
-      return error[prop] as string;
-    }
-  }
-
-  return UNEXPECTED_ERROR_MESSAGES.DefaultMessage;
 }
-
