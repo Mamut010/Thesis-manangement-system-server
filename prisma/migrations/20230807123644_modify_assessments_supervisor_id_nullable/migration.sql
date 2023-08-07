7d57de3d1af05757cfc402fabff25543fd67f12e
Mamut010 <phucthinh0013@gmail.com>
2023-08-07 19:47:46 +0700

    Added unexpected missing prisma migration file

---
+++ prisma/migrations/20230807123644_modify_assessments_supervisor_id_nullable/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `proposedDate` on the `OralDefenseRegistration` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `actualDate` on the `OralDefenseRegistration` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `receivingDate` on the `OralDefenseRegistration` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `submissionDate` on the `OralDefenseRegistration` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `submissionDeadline` on the `Thesis` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `BachelorThesisAssessment` MODIFY `supervisor1Id` INTEGER NULL,
+    MODIFY `supervisor2Id` INTEGER NULL;
+
+-- AlterTable
+ALTER TABLE `OralDefenseAssessment` MODIFY `supervisor1Id` INTEGER NULL,
+    MODIFY `supervisor2Id` INTEGER NULL;
+
+-- AlterTable
+ALTER TABLE `OralDefenseRegistration` MODIFY `proposedDate` DATETIME NULL,
+    MODIFY `actualDate` DATETIME NULL,
+    MODIFY `receivingDate` DATETIME NULL,
+    MODIFY `submissionDate` DATETIME NULL;
+
+-- AlterTable
+ALTER TABLE `Thesis` MODIFY `submissionDeadline` DATETIME NULL;
