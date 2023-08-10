7b714cf89fbc8eada1c249e1daf1505b0b8b40bd
Mamut010 <phucthinh0013@gmail.com>
2023-08-11 01:19:29 +0700

    Refactored from using momentjs to using luxon

--- src/lib/form-filler/form-fields/date-field.ts
+++ src/lib/form-filler/form-fields/date-field.ts
@@ -1,8 +1,14 @@
-import moment from "moment";
+import { DateTime } from "luxon";
 import { TextField } from "./text-field";
+import { DATETIME_FORMATS } from "../../../contracts/constants/datetime-formats";
 
 export class DateField extends TextField {
-    constructor(public name: string, public date?: Date | string, public format?: string) {
-        super(name, moment(date).format(format));
+    constructor(
+        public name: string, 
+        public date?: Date | string, 
+        public format: Intl.DateTimeFormatOptions = DATETIME_FORMATS.DATE_LONG) {
+        date = typeof date === 'string' ? new Date(date) : date;
+        const value = typeof date !== 'undefined' ? DateTime.fromJSDate(date).toLocaleString(format) : undefined;
+        super(name, value);
     }
 }
