a661aeaefda451fcd1ad35f463228963da039f60
Mamut010 <phucthinh0013@gmail.com>
2023-08-10 22:25:47 +0700

    Updated Form filler API

--- src/lib/form-filler/index.ts
+++ src/lib/form-filler/index.ts
@@ -5,6 +5,7 @@ export * from './interfaces/form-filler.interface';
 export * from './types/form-field-handle-options';
 export * from './types/form-fill-request';
 export * from './types/path-or-typed-array';
+export * from './types/supported-image-type';
 
 export * from './form-field-handlers/pdf-form-field-handler';
 
@@ -12,3 +13,6 @@ export * from './form-fillers/pdf-form-filler';
 
 export * from './form-fields/radio-button-field';
 export * from './form-fields/text-field';
+export * from './form-fields/check-box-field';
+export * from './form-fields/date-field';
+export * from './form-fields/image-button-field';
