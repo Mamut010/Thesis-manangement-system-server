c83261bc61eccb16978414c1284e24ae73ef0bb8
Mamut010 <phucthinh0013@gmail.com>
2023-08-11 13:10:24 +0700

    Added body parser middleware and relocated some files

---
+++ src/core/constants/content-types.ts
@@ -0,0 +1,10 @@
+export const CONTENT_TYPES = {
+    Any: '*/*',
+    ApplicationAny: 'application/*',
+    ApplicationPdf: 'application/pdf',
+    ApplicationJson: 'application/json',
+    ApplicationOctetStream: 'application/octet-stream',
+    TextPlain: 'text/plain',
+    TextHtml: 'text/html',
+    MultipartFormData: 'multipart/form-data',
+} as const;
