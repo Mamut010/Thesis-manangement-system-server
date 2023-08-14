484a9e54b88b5110be2ac69d2fcf06a7f9860a58
Mamut010 <phucthinh0013@gmail.com>
2023-08-14 22:57:42 +0700

    Updated inversify adapter to not spawn child container whenever a dependency is to be resolved

--- src/core/ioc-adapters/inversify.adapter.ts
+++ src/core/ioc-adapters/inversify.adapter.ts
@@ -5,7 +5,6 @@ export class InversifyAdapter implements IocAdapter {
   constructor(private readonly container: Container) {}
 
   get<T>(someClass: ClassConstructor<T>, action?: Action): T {
-    const childContainer = this.container.createChild();
-    return childContainer.resolve<T>(someClass);
+    return this.container.resolve<T>(someClass);
   }
 }
