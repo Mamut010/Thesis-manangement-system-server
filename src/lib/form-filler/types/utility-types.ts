e1fbd5d0677b9b81acd046ee1f9f8848b7cb6a3a
Mamut010 <phucthinh0013@gmail.com>
2023-08-17 11:49:08 +0700

    Add line break to improve readability

--- src/lib/form-filler/types/utility-types.ts
+++ src/lib/form-filler/types/utility-types.ts
@@ -1,6 +1,7 @@
 import { SUPPORTED_IMAGE_TYPES } from "../constants/images";
 
 export type SupportedImageType = typeof SUPPORTED_IMAGE_TYPES[keyof typeof SUPPORTED_IMAGE_TYPES];
+
 export interface NumberFieldOptions {
     locale?: string, 
     format?: Intl.NumberFormatOptions,
