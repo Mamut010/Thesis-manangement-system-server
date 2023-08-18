3475e11f36c268fe9d4f65c598e1d0e9c6a34dc6
Mamut010 <phucthinh0013@gmail.com>
2023-08-18 12:15:45 +0700

    Fixed missing implements phrase in UuidService definition

--- src/shared/services/uuid.service.ts
+++ src/shared/services/uuid.service.ts
@@ -2,9 +2,10 @@ import { injectable } from "inversify";
 import getUuidByString from "uuid-by-string";
 import { SupportedUuidVersion } from "../types/uuid";
 import { v4 } from "uuid";
+import { UuidServiceInterface } from "../interfaces";
 
 @injectable()
-export class UuidService {
+export class UuidService implements UuidServiceInterface {
     generate(seed?: any, version?: SupportedUuidVersion) {
         if (seed === null || typeof seed === 'undefined') {
             seed = v4();
