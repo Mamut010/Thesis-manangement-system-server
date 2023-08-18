81cfbf275bac9e3408ce51fb0494575810f19f33
Mamut010 <phucthinh0013@gmail.com>
2023-08-19 01:35:34 +0700

    Removed redundant if check in sleepThenCallback()

--- src/utils/timer-helpers.ts
+++ src/utils/timer-helpers.ts
@@ -12,7 +12,5 @@ export async function sleepThenValue<T>(ms: number, value: T, options?: Abortabl
 export async function sleepThenCallback<T>(ms: number, callback: () => Promise<T> | T, options?: Abortable)
     : Promise<T | undefined> {
     await setTimeout(ms, undefined, options);
-    if (!options?.signal?.aborted) {
-        return await callback();
-    }
+    return await callback();
 }
