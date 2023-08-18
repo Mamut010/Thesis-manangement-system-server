73469c1d56fc99219d845047627653e227496d8b
Mamut010 <phucthinh0013@gmail.com>
2023-08-18 11:52:49 +0700

    Changed timer helpers's syntax from arrow to normal function

--- src/utils/timer-helpers.ts
+++ src/utils/timer-helpers.ts
@@ -1,18 +1,16 @@
 import { Abortable } from 'events';
 import { setTimeout } from 'timers/promises';
 
-export const sleep = async (ms: number, options?: Abortable)
-    : Promise<void> => {
+export async function sleep(ms: number, options?: Abortable): Promise<void> {
     await setTimeout(ms, undefined, options);
 }
 
-export const sleepThenValue = async <T>(ms: number, value: T, options?: Abortable)
-    : Promise<T> => {
+export async function sleepThenValue<T>(ms: number, value: T, options?: Abortable): Promise<T> {
     return setTimeout(ms, value, options);
 }
 
-export const sleepThenCallback = async <T>(ms: number, callback: () => Promise<T> | T, options: Abortable)
-    : Promise<T | undefined> => {
+export async function sleepThenCallback<T>(ms: number, callback: () => Promise<T> | T, options: Abortable)
+    : Promise<T | undefined> {
     await setTimeout(ms, undefined, options);
     if (!options.signal?.aborted) {
         return await callback();
