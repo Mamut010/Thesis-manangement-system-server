d1895d491b5864e886f128b1cc6a19009ac0eb57
Mamut010 <phucthinh0013@gmail.com>
2023-09-12 01:05:28 +0700

    Updated Workflow API

--- src/api/others/workflow/action-handlers/handlers/request-admin-group-action-handler.ts
+++ src/api/others/workflow/action-handlers/handlers/request-admin-group-action-handler.ts
@@ -1,43 +1,18 @@
-import { ActionHandlerInput, ActionHandlerOutput } from "../types";
-import { BaseRequestActionHandler } from "../bases/base-request-action-handler";
 import { NotificationServiceInterface } from "../../../../../shared/interfaces";
 import { STORED_REQUEST_DATA_KEYS } from "../../constants/request-data-keys";
-import { NotFoundError } from "../../../../../contracts/errors/not-found.error";
-import { ERROR_MESSAGES } from "../../../../../contracts/constants/error-messages";
-import { getTitleAndContentFromData } from "../../utils/action-handler-helpers";
-import { removeDuplicates } from "../../../../../utils/array-helpers";
 import { inject, injectable } from "inversify";
 import { INJECTION_TOKENS } from "../../../../../core/constants/injection-tokens";
 import { GroupRepoInterface, RequestDataRepoInterface } from "../../../../../dal/interfaces";
 import { WorkflowRequestDataProcessorInterface } from "../../request-data-processor";
+import { GroupRequestActionHandler } from "./group-request-action-handler";
 
 @injectable()
-export class RequestAdminGroupActionHandler extends BaseRequestActionHandler {
+export class RequestAdminGroupActionHandler extends GroupRequestActionHandler {
     constructor(
         @inject(INJECTION_TOKENS.RequestDataRepo) requestDataRepo: RequestDataRepoInterface,
         @inject(INJECTION_TOKENS.WorkflowRequestDataProcessor) requestDataProcessor: WorkflowRequestDataProcessorInterface,
-        @inject(INJECTION_TOKENS.GroupRepo) private groupRepo: GroupRepoInterface,
-        @inject(INJECTION_TOKENS.NotificationService) private notificationService: NotificationServiceInterface) {
-        super(requestDataRepo, requestDataProcessor, STORED_REQUEST_DATA_KEYS.AdminGroup);
-    }
-
-    protected async sendRequest(dataValue: string, actionInput: ActionHandlerInput): Promise<ActionHandlerOutput> {
-        const group = await this.groupRepo.findOneById(dataValue);
-        if (!group) {
-            throw new NotFoundError(ERROR_MESSAGES.NotFound.GroupNotFound);
-        }
-
-        await Promise.all(group.memberIds.map(userId => {
-            return this.notificationService.sendNotification({
-                senderId: actionInput.actionerId,
-                receiverId: userId,
-                ...getTitleAndContentFromData(actionInput.data),
-            })
-        }));
-
-        return {
-            requestUsers: actionInput.requestUsers,
-            resolvedUserIds: removeDuplicates(group.memberIds.concat(actionInput.actionerId))
-        };
+        @inject(INJECTION_TOKENS.GroupRepo) groupRepo: GroupRepoInterface,
+        @inject(INJECTION_TOKENS.NotificationService) notificationService: NotificationServiceInterface) {
+        super(requestDataRepo, requestDataProcessor, STORED_REQUEST_DATA_KEYS.AdminGroup, groupRepo, notificationService);
     }
 }
